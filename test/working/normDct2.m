415144041b8f9f9585166402832e35fed100d51b
Tanguy Poret <tanguy.poret@hotmail.fr>
2016-05-04 08:15:37 +0200

    split function for DCT
    It does the 'same', one apply DCT on a vector of patches, while the other apply a DCT2 directly to the patches (althought there is more step to achieve the later)

--- test/working/normDct2.m
+++ test/working/normDct2.m
@@ -1,22 +1,27 @@
-function [ normVector ] = normDct( vPatch, doNorm )
-%NORMDCT Apply dct + zero mean and variance
+function [ normVector ] = normDct2( vPatch, pSize, doNorm )
+%NORMDCT Apply zero mean and variance + dct2
 %   Reconstruct the patch from a vector and apply normalization to have
 %   zero mean and unit variance.
 %   Return the patch as a vector
 %   VPATCH the patch to be treated
-if nargin ~= 2
+%   PSIZE the size of the patch (always a square)
+if nargin ~= 3
     error('Not enough argument')
 end
 
-X = double(vPatch);
+patch = reshape(vPatch, [pSize pSize]);
+
+X = double(patch);
 if doNorm
     % Zero mean
     X = X-mean(X(:));
     % Zero variance
     X = X/std(X(:));
 end
+% Apply dct2
+X = dct2(double(X));
 
-normVector = dct(double(X));
+normVector = reshape(X, [pSize*pSize 1]);
 
 end
 
