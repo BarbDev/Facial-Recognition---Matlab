0124e003addd54eb57b4e305d54e1c9085653dae
Tanguy Poret <tanguy.poret@hotmail.fr>
2016-05-04 08:12:02 +0200

    DCT variation demo

--- test/tests/finalTestv2.m
+++ test/tests/finalTestv2.m
@@ -20,34 +20,62 @@ overflow = 3;
 
 patches = getPatches(R, pSize, overflow);
 
-%% On normalise les patches pour avoir 'zero mean and unit variance'
-normPatch = patches;
-[vSize, nbrFeature] = size(normPatch);
+
+patchNormDCT2 = patches;
+patchNormDCT = patches;
+patchDCT2 = patches;
+patchDCT = patches;
+[vSize, nbrFeature] = size(patchNormDCT2);
 for i = 1:nbrFeature
-    normPatch(:,i) = normDct(normPatch(:,i), pSize);
+    %% On normalise les patches pour avoir 'zero mean and unit variance'
+    patchNormDCT2(:,i) = normDct2(patchNormDCT2(:,i), pSize, true);
+    patchNormDCT(:,i) = normDct(patchNormDCT(:,i), true);
+    %% On ne normalise pas (pour comparaison)
+    patchDCT2(:,i) = normDct2(patchDCT2(:,i), pSize, false);
+    patchDCT(:,i) = normDct(patchDCT(:,i), false);
 end
 
-%% 'select the 15 lower frequency components of the DCT coefficients'
-% Pour chaque patch on ne garde que les 15 coef les + petits (c ça?)
-for i = 1:nbrFeature
-    vector = sort(normPatch(:,i));
-    % Ici on enlève tous les 0 et on garde les 15 plus petit
-    % ??? Que faire s'il y a moins de 15coef supérieur à 0?
-    %%% Que faut il choisir ? doublon ou pas ?
-    %vector = nonzeros(vector); % On garde que les coef > 0
-    vector = unique(vector); % On garde que les coefs > 0 et pas de doublon
-    if size(vector) < 15 % On garde les 15 premier ou -
-        vector = vector(1:end);
-    else
-        vector = vector(1:15);
-    end
-    %TODO Bug, pas la même taille...
-    normPatch(:,i) = vector;
-end
+% %% 'select the 15 lower frequency components of the DCT coefficients'
+% % Pour chaque patch on ne garde que les 15 coef les + petits (c ça?)
+% for i = 1:nbrFeature
+%     vector = sort(normPatch(:,i));
+%     % Ici on enlève tous les 0 et on garde les 15 plus petit
+%     % ??? Que faire s'il y a moins de 15coef supérieur à 0?
+%     %%% Que faut il choisir ? doublon ou pas ?
+%     %vector = nonzeros(vector); % On garde que les coef > 0
+%     vector = unique(vector); % On garde que les coefs > 0 et pas de doublon
+%     if size(vector) < 15 % On garde les 15 premier ou -
+%         vector = vector(1:end);
+%     else
+%         vector = vector(1:15);
+%     end
+%     %TODO Bug, pas la même taille...
+%     normPatch(:,i) = vector;
+% end
 
 
-%% Affichage des résultats
-imgF = getImgFromPatches(patches, size(R), overflow);
-imshow(imgF)
-figure
-imshow(normPatch)
+%% Affichage résultat recontruction image à partir des patchs
+figure('Name', 'DCT & DCT2 comparison')
+subplot(3,2,[1 2])
+imgPatch = getImgFromPatches(patches, size(R), overflow);
+imshow(imgPatch)
+
+%% Affichage résultats DCT
+imgPatchDCT = getImgFromPatches(patchDCT, size(R), overflow);
+subplot(3,2,3)
+imshow(imgPatchDCT)
+title('DCT without normalization')
+imgPatchNormDCT = getImgFromPatches(patchNormDCT, size(R), overflow);
+subplot(3,2,4)
+imshow(imgPatchNormDCT)
+title('DCT with normalization')
+
+%% Affichage résultats DCT2
+imgPatchDCT2 = getImgFromPatches(patchDCT2, size(R), overflow);
+subplot(3,2,5)
+imshow(imgPatchDCT2)
+title('DCT2 without normalization')
+imgPatchNormDCT2 = getImgFromPatches(patchNormDCT2, size(R), overflow);
+subplot(3,2,6)
+imshow(imgPatchNormDCT2)
+title('DCT2 with normalization')
