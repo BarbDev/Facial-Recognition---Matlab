7e5828c632d583c1d46ba21d93be553afa5d486a
Tanguy Poret <tanguy.poret@hotmail.fr>
2016-05-31 12:16:41 +0200

    Major improvement now adapted for several database till they conform to a certain form of folders and files organization

--- test/working/getDataset.m
+++ test/working/getDataset.m
@@ -1,19 +1,61 @@
-function [ D ] = getDataset( )
-%GETLOWFREQCOMP Summary of this function goes here
-%   Detailed explanation goes here
+function [ D ] = getDataset( datasetName )
+%getDat Get one of the available database
+%   This function return a struct containing the image from the wished
+%   database.
+%   Argument:
+%   NAME the name of the database to get
+%      -chokepoint, colorferet, CroppedYale, ExtendedYaleB, ORL
+%      Or put 'null' in case you want to select a database which is outside
+%      the current folder.
 
-% On a le repétoire de base
-% On précise le répertoire de dataset
-datasetName = 'database';
-currDir = pwd;
+%% Getting the supported file formats
+form = imformats;
+expression = '\w\.(';
+for i = 1:size(form,2)
+    if size(form(i).ext,2) > 1 % We have different extension for same format
+        for j = 1:size(form(i).ext,2)
+            if i~=1 || j~=1
+                expression = strcat(expression,'|',form(i).ext(j));
+            else
+                expression = strcat(expression,form(i).ext(j));
+            end
+        end
+    else
+        if i~=1
+            expression = strcat(expression,'|',form(i).ext);
+        else
+            expression = strcat(expression,form(i).ext);
+        end
+    end
+end
+expression = strcat(expression,')$');
 
+%% Setting some default variable
+% Adapt the separator according to the user system
 if strcmp(getenv('OS'),'Windows_NT')
     separator = '\';
 else
     separator = '/';
 end
-datasetPath = [currDir separator datasetName];
+if regexp(datasetName, '^(chokepoint|colorferet|CroppedYale|ExtendedYaleB|ORL)$')
+    datasetPath = [pwd separator 'database' separator 'dataset_' datasetName];
+    if strcmp(datasetName, 'chokepoint')
+        datasetPath = uigetdir(datasetPath,'Choose a database');
+    elseif strcmp(datasetName, 'colorferet')
+        error('Cannot use colorferet, not implemented yet')
+        return;
+    end
+elseif strcmpi(datasetName, 'null')
+    datasetPath = uigetdir(pwd,'Choose a database');
+else
+    error(['Database name: %s \nPlease check spelling (case sensitive)' ...
+        'and your current folder if not using ''null'''], datasetName);
+    return;
+end
 
+% Generic database case - means that the database is constructed as follow:
+% The current directory contains several folders (class) which has
+% different face shot.
 % Contenu du dataset
 datasetContent = dir(datasetPath);
 dirCount = 1;
@@ -26,13 +68,16 @@ for i = 1:size(datasetContent)
         % On boucle maintenant dans le dossier contenant les images
         currentDirName = [datasetPath separator objName];
         currentDir = dir(currentDirName);
-        
+
         D(dirCount).name = datasetContent(i).name; % TODO: pre-allocation pour meilleur perf
-        for j = 3:size(currentDir) % pas de -2, on boucle jusqu'a la fin de l'index
-            D(dirCount).set(j-2).name = currentDir(j).name;
-            D(dirCount).set(j-2).img(:,:) = double(imread([currentDirName separator D(dirCount).set(j-2).name]));
+        fileCount = 1;
+        for j = 1:size(currentDir)
+            if ~cellfun(@isempty, regexpi(currentDir(j).name, expression, 'start')) % si c une image
+                D(dirCount).set(fileCount).name = currentDir(j).name;
+                D(dirCount).set(fileCount).img(:,:) = double(imread([currentDirName separator D(dirCount).set(fileCount).name]));
+                fileCount = fileCount + 1;
+            end
         end
-        %D(:,:,:,dirCount) = imageSet;
         dirCount = dirCount + 1;
     end
 end
