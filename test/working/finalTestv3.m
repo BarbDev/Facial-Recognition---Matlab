b9f202b8203c0d497e5dcaa0ddae87edbc78bfcc
Tanguy Poret <tanguy.poret@hotmail.fr>
2016-05-31 12:14:36 +0200

    in stand-by TODO: test with the dictionnary if I finally manage to generate sparse code

--- test/working/finalTestv3.m
+++ test/working/finalTestv3.m
@@ -13,9 +13,18 @@ overflow = 3;
 
 Dict = getDataset();
 
+% X = ones(size(Dict(1).set(1).img,1)*size(Dict(1).set(1).img,2), size(Dict,2)*size(Dict(1).set,2));
+
 for i = 1:size(Dict,2) % Pour chaque 'class d'image'
     for j = 1:size(Dict(i).set, 2) % Pour chaque image d'une classe
         Rs = getRegions(Dict(i).set(j).img);
+        
+%         % Range tte les images en une grande matrice
+%         tempImg = Dict(i).set(j).img;
+%         index = size(Dict(i).set, 2)*(i-1)+j;
+%         X(:,index) = tempImg(:);
+        
+        
         for k = 1:4 % Pour chaque région d'une image
             patches = getPatches(Rs(:,:,k), pSize, overflow);
             patchNormDCT2 = normDct2(patches, true, true);
@@ -36,6 +45,10 @@ for i = 1:size(Dict,2) % Pour chaque 'class d'image'
         end
     end
 end
+
+% % On test le dictionnaire
+% opt = struct('K',32, 'samet','mexomp','saopt',struct('tnz',4));
+% Ds = dictlearn_mb('X',X,opt);
 
 %%% Computing the image similarities used the methods from LSED [24]
 % simi -> computed similaritie, assumed the lower the better the
